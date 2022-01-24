0e53fb044f83c166369396f47a5bfac88d61e81a
ccchiml <97814760+ccchiml@users.noreply.github.com>
2022-01-24 15:37:14 +0800

    Add files via upload

---
+++ router.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "router.h"
+LL Getinput()
+{
+	pf = fopen("date.txt", "r+");
+	if (pf == NULL)
+	{
+		return 0;
+	}
+	return 1;
+}
+LL Getrule()
+{
+	rf = fopen("rule.txt", "r");
+
+}
